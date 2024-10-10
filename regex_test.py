5f004cfaefccf70d4465cc45041fd72ecce5dd7f
Lucas Rizzo <lucas.rizzo@tudublin.ie>
2024-10-10 17:07:15 +0100

    Data cleaned and rules generated for all data.

--- regex_test.py
+++ regex_test.py
@@ -2,20 +2,47 @@ import re
 
 # pattern = r'(\d+)?\s*([<>]=?)?\s*("[\w\s-]+"|\w+)\s*([<>]=?)?\s*(\d+)?'
 # pattern = r'(\d+)?\s*([<>]=?)?\s*((?:"[\w\s-]+")|(?:\'[\w\s-]+\'|\w+(?:-?\w+)*))\s*([<>]=?)?\s*(\d+)?'
-pattern = r'(-?\d+)?\s*([<>]=?)?\s*((?:"[\w\s-]+")|(?:\'[\w\s-]+\'|\w+(?:-?\w+)*))\s*([<>]=?)?\s*(-?\d+)?'
+pattern = r'(-?\d+(?:\.\d+)?|\w+)\s*([<>]=?)\s*(-?\d+(?:\.\d+)?|\w+)(?:\s*([<>]=?)\s*(-?\d+(?:\.\d+)?))?'
 
 # Your input string
-input_string = "-10 <= relationship <= -2909"
+input_string = "0.457 <= Bilirubin"
 
 # Match the pattern
 match = re.match(pattern, input_string)
 
 if match:
-    number1 = int(match.group(1)) if match.group(1) else None
-    op1 = match.group(2) if match.group(2) else None
-    word = match.group(3)
-    op2 = match.group(4) if match.group(4) else None
-    number2 = int(match.group(5)) if match.group(5) else None
+
+    groups = match.groups()
+    
+    # Using any() to check if any group is None
+    # Expression does do have 5 elements. Can be value (<, <=) word, or word (<, <=) value
+    if any(group is None for group in groups):
+
+        try: # value (<, <=) word
+            number1 = float(groups[0])
+            op1 = groups[1]
+            word = groups[2]
+            op2 = None
+            number2 = None
+        except ValueError:
+            
+            try: # word (<, <=) value
+                word = groups[0]
+                op1 = groups[1]
+                number1 = float(groups[2])
+                op2 = None
+                number2 = None
+            except ValueError:
+                raise("Expression not identified")
+                exit()
+
+
+    else: # value (<, <=) word (<, <=) value
+        number1 = float(match.group(1)) if match.group(1) else None
+        op1 = match.group(2) if match.group(2) else None
+        word = match.group(3)
+        op2 = match.group(4) if match.group(4) else None
+        number2 = float(match.group(5)) if match.group(5) else None
 
     print(input_string)
     print("First number:", number1)
--- rule.py
+++ rule.py
@@ -62,9 +62,55 @@ class Rule(object):
         output_value = rules_df.loc[self.index, "Output value"]
 
         for conclusion in conclusion_set:
-            if conclusion[1] == output_value:
+            if conclusion[0] == output_value:
                 rule += f" -> {conclusion[0]
                                } [{conclusion[1]}, {conclusion[2]}]"
                 break
+            else:
+                print(conclusion[0], output_value)
 
         return rule
+    
+    def get_rule_str(self, rules_df: pd.core.frame.DataFrame, conclusion_set: list) -> str:
+        rule = ""
+        features_full = list()  # List of features used
+        for premise in self.conditions:
+            # Append all features used so it is possible count their frequency
+            features_full.append(premise.feature)
+
+        features_set = set(features_full)  # Unique features employed
+
+        for feature in features_set:
+
+            total = features_full.count(feature)
+
+            if total == 1:  # If feature has been used only once it ends with " AND "
+                for premise in self.conditions:
+                    if premise.feature == feature:
+                        rule += f"\"{premise.name} {premise.feature}\" AND "
+                        break
+            else:  # If feature has been used more than once,
+                # it needs to concatenate all possibilities with " OR "
+                rule += f"("
+                for premise in self.conditions:
+                    if premise.feature == feature:
+                        rule += f"\"{premise.name} {premise.feature}\" OR "
+                # Remove last " OR " and add new " AND "
+                rule = rule[:-4] + ") AND "
+
+        rule = rule[:-5]  # Remove last " AND "
+        try:
+            output_value = rules_df.loc[self.index, "Output value"]
+        except KeyError:
+            print("Output value not found")
+            print(rules_df.loc[self.index,])
+            input("Pause...")
+
+        for conclusion in conclusion_set:
+            if conclusion[0] == output_value:
+                rule += f" -> {conclusion[0]
+                               } [{conclusion[1]}, {conclusion[2]}]"
+                break
+           
+        return rule
+
